3499fab287f9fa433b2ae37158b4cf36c3b2b21e
SunshineMocha <127298037+SunshineMocha@users.noreply.github.com>
2023-04-26 12:12:45 +0200

    more preparation

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ console.log("Hello Pokemon World");
 
 let actualPage; // per gestire le pagine
 
-PokeService.getNextPokemon(actualPage).then(pokemon => {
-    console.log(pokemon);
-}).catch (err => {
-    console.log("Unable to get the pokemon")
-});
+// PokeService.getNextPokemon(actualPage).then(pokemon => {
+//     actualPage = pokemon;
+//     console.log(actualPage);
+// }).catch (err => {
+//     console.log("Unable to get the pokemon")
+// });
+
+loadNextPage();
+
+function loadNextPage(){
+    PokeService.getNextPokemon(actualPage).then(pokemonPage => {
+        actualPage = pokemonPage;
+        console.log(actualPage);
+    }).catch (err => {
+        console.log("Unable to get the pokemon")
+    });
+}
+
+function loadPreviousPage(){
+    PokeService.getPreviousPokemon(actualPage).then(pokemonPage => {
+        actualPage = pokemonPage;
+        console.log(actualPage);
+    }).catch (err => {
+        console.log("Unable to get the pokemon")
+    });
+}
--- service/poke-service.js
+++ service/poke-service.js
@@ -3,11 +3,18 @@ class PokeService{
     static api_URL = 'https://pokeapi.co/api/v2/pokemon';
 
     static getNextPokemon(page){
-        if (page) {
-            
+        if (page === undefined) {
+           return fetch(this.api_URL).then(resp => resp.json());
         } else {
-            return fetch(this.api_URL).then(resp => resp.json());
+            return fetch(page.next).then(resp => resp.json());
         }
     }
-    
+    static getPreviousPokemon(page){
+        if (page === undefined || page.previous === null) {
+           return fetch(this.api_URL).then(resp => resp.json());
+        } else {
+            return fetch(page.previous).then(resp => resp.json());
+        }
+    }
+
 }
